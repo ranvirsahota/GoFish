5e502661b5dec8aabc9dfa586c77629c6a7da2f5
Ranvir <rsahota666@gmail.com>
2019-07-01 21:44:18 +0100

    Inital commit. SmartCardPlayer selects card to fish by looking for match with card in hand against cards that publicly known to be held by players which would also helps to select who to fish from.

---
+++ SmartCardPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoFish
+{
+    class SmartCardPlayer : AI
+    {
+        public SmartCardPlayer(string name) : base(name){}
+
+        public override void Decision(bool isMyTurn, string cardSeeking, string fishFrom)
+        {
+            foreach (KeyValuePair<String, int> cardType in _cards)
+            {
+                foreach (KeyValuePair<string, List<string>> public_cards_of_cardplayer in Globals.PUBLICLY_KNOWN_CARDS)
+                {
+                    if (!public_cards_of_cardplayer.Key.Equals(Name))
+                    {
+                        foreach (string public_known_card in public_cards_of_cardplayer.Value)
+                        {
+                            if (cardType.Key.Equals(public_known_card))
+                            {
+                                cardSeeking = public_known_card;
+                                fishFrom = public_cards_of_cardplayer.Key;
+                                break;
+                            }
+                        }
+                    }
+                    if (!(cardSeeking == null && fishFrom == null)) { break; }
+                }
+                if (!(cardSeeking == null && fishFrom == null)) { break; }
+            }
+            base.Decision(isMyTurn, cardSeeking, fishFrom);
+        }
+    }
+}
+ 
