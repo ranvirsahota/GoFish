0c3ad3c2d9bbfaca51e689c11ae471266676d896
Ranvir <rsahota666@gmail.com>
2019-07-01 21:31:48 +0100

    Inital commit. DumbCardPlayer Decision method calls overidden decision method in AI to make a decision on card to fish for and from whom.

---
+++ DumbCardPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoFish
+{
+    //set memory capacity to 0
+    class DumbCardPlayer : AI
+    {
+        public DumbCardPlayer(String name) : base(name) {}
+
+        public override void Decision(bool isMyTurn, string cardSeeking, string fishFrom)
+        {
+            base.Decision(isMyTurn, cardSeeking, fishFrom);
+        }
+    }
+}
