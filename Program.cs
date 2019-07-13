05ed071fd23339c716a89936d7ffce9b703da080
Ranvir <rsahota666@gmail.com>
2019-07-13 15:42:40 +0100

    updated method name due to new code conventions

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace GoFish
 {
     class Program
     {
-        static void Main(string[] args)
+        static void main(string[] args)
         {
             GameSetup gameSetup = new GameSetup();
             RunGame runGame = new RunGame(gameSetup.cardPlayers);
