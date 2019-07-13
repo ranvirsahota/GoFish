2b2fda3a0314b820d00fd5826590f94f730e7c5e
Ranvir <rsahota666@gmail.com>
2019-07-13 15:40:53 +0100

    makeAI instantiate and returns a type of AI based upon user input

--- AIFactory.cs
+++ AIFactory.cs
@@ -6,6 +6,17 @@ namespace GoFish
 {
     class AIFactory
     {
-        public AIFactory() {}
+        public AI makeAI(string AIType, string AIName)
+        {
+            if (AIType.Equals("Dumb AI"))
+            {
+                return new DumbCardPlayer(AIName);
+            }
+
+            else
+            {
+                return new SmartCardPlayer(AIName);
+            }
+        }
     }
 }
