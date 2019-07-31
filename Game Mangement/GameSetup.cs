a9f9fb4c1e657d5b618a7fb016d07ecd79d5966d
Ranvir <rsahota666@gmail.com>
2019-07-31 14:39:54 +0100

    Created folder to help improve structure

---
+++ Game Mangement/GameSetup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoFish
+{
+    class GameSetup
+    {
+        public List<string> cards;
+        public Dictionary<string, CardPlayer> cardPlayers;
+
+        private CardPlayerFactory cardPlayerFactory;
+        public GameSetup()
+        {
+            cardPlayers = new Dictionary<string, CardPlayer>();
+            cardPlayerFactory = new CardPlayerFactory();           
+            createCardPlayers("Smart AI", "Jack");
+            createCardPlayers("Dumb AI", "Richard");
+            createCardPlayers("Player", "Bill");
+            createCardPlayers("Smart AI", "Jake");
+
+            createCards();
+            shufflecards();
+            handCards();
+            makeStack();
+
+        }
+
+        private void createCards()
+        {
+            cards = new List<string>()
+            {
+                "ace", "ace", "ace", "ace",
+                "two", "two", "two", "two",
+                "three", "three", "three", "three",
+                "four", "four", "four", "four",
+                "five", "five", "five", "five",
+                "six", "six", "six", "six",
+                "nine", "nine", "nine", "nine",
+                "ten", "ten", "ten", "ten",
+                "jack", "jack", "jack", "jack",
+                "queen", "queen", "queen", "queen",
+                "king", "king", "king", "king",
+                "seven", "seven", "seven", "seven",
+                "eight", "eight", "eight", "eight"
+            };
+        }
+
+        private void shufflecards() { cards = cards.OrderBy(x => new Random().Next()).ToList<string>(); }
+
+        private void createCardPlayers(string cardPlayerType, string cardPlayerName)
+        {
+            cardPlayers.Add(cardPlayerName, cardPlayerFactory.makeCardPlayer(cardPlayerType, cardPlayerName));
+        }
+
+        private void handCards()
+        {
+            for (int counter = 1; counter <= 5; counter++)
+            {
+                Console.WriteLine(counter);
+                foreach (KeyValuePair<string, CardPlayer> cardPlayer in cardPlayers)
+                {
+                    Console.WriteLine(String.Format("{0} goes to {1}", cards[0], cardPlayer.Key));
+
+                    cardPlayer.Value.CardsSet(cards[0]);
+                    cards.RemoveAt(0);
+                }
+            }
+        }
+
+        private void makeStack()
+        {
+            Globals.STACK = cards;
+        }
+    }
+}
---
+++ Game Mangement/RunGame.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoFish
+{
+    class RunGame
+    {
+        public RunGame(Dictionary<String, CardPlayer> cardPlayers)
+        {
+
+            CardPlayer currentWinner = null;
+            do
+            {
+                foreach (KeyValuePair<string, CardPlayer> cardPlayer in cardPlayers)
+                {
+                    if (currentWinner == null) { currentWinner = cardPlayer.Value; }
+
+                    if (currentWinner.Books.Count < cardPlayer.Value.Books.Count) { currentWinner = cardPlayer.Value; }
+                    for (int i = 0; i < 1;)
+                    {
+                        string cardSeeking = null;
+                        int cardSeekingAmount = 0;
+
+                        if (cardPlayer.Value.CardCount == 0)
+                        {
+                            if (Globals.STACK.Count >= 1)
+                            {
+                                cardPlayer.Value.CardsSet(Globals.STACK[0]);
+                                Console.WriteLine(cardPlayer.Key + "got a: " + Globals.STACK[0]);
+                                Globals.PUBLICLY_KNOWN_CARDS[cardPlayer.Key].Add(Globals.STACK[0]);
+                                Globals.STACK.Remove(Globals.STACK[0]);
+                            }
+                            else
+                            {
+                                Globals.CARD_PLAYER_NAMES.Remove(cardPlayer.Key);
+                                Globals.PUBLICLY_KNOWN_CARDS.Remove(cardPlayer.Key);
+                                break;
+                            }
+                        }
+
+                        cardPlayer.Value.Decision(true);
+
+                        cardSeeking = Globals.CARD_TRANSACTION.Keys.First();
+                        if (!Globals.PUBLICLY_KNOWN_CARDS[cardPlayer.Key].Contains(cardSeeking)) { Globals.PUBLICLY_KNOWN_CARDS[cardPlayer.Key].Add(cardSeeking); }
+                        cardPlayers[Globals.CARD_PLAYERS_FROM_TO_TRANSACTION[1]].Decision(false, cardSeeking);
+
+                        cardSeekingAmount = Globals.CARD_TRANSACTION[Globals.CARD_TRANSACTION.Keys.First()];
+
+                        if (cardSeekingAmount > 0) { cardPlayer.Value.CardsSet(cardSeeking, cardSeekingAmount); }
+
+                        else
+                        {
+                            if (Globals.STACK.Count >= 1)
+                            {
+                                cardPlayer.Value.CardsSet(Globals.STACK[0]);
+                                Console.WriteLine("I got a: " + Globals.STACK[0]);
+                                Globals.PUBLICLY_KNOWN_CARDS[cardPlayer.Key].Add(Globals.STACK[0]);
+                                if (Globals.STACK[0] != cardSeeking)
+                                {
+                                    i++;
+                                    Console.WriteLine("Not a match");
+                                }
+                                Globals.STACK.Remove(Globals.STACK[0]);
+                            }
+                            else
+                            {
+                                i++;
+                            }
+                        }
+
+                        Console.ReadLine();
+
+                        Globals.CARD_TRANSACTION.Remove(cardSeeking);
+                        Globals.CARD_PLAYERS_FROM_TO_TRANSACTION[1] = null;
+                        Globals.CARD_PLAYERS_FROM_TO_TRANSACTION[0] = null;
+                    }
+                }
+            } while (Globals.CARD_PLAYER_NAMES.Count > 1);
+            Console.WriteLine("Card Player: " + currentWinner.Name);
+            Console.WriteLine("Card in stack: " + Globals.STACK.Count);
+            Console.WriteLine(String.Format("{0} has {1} books", currentWinner.Name, currentWinner.Books.Count));
+            Console.ReadLine();
+        }
+    }
+}
