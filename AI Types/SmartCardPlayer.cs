c9870fa340d6f05f0516651b2c3467ce155ee7a5
Ranvir <rsahota666@gmail.com>
2019-07-31 14:38:52 +0100

    Created folder AI Types to help imporve sturcture and organization

---
+++ AI Types/SmartCardPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoFish
+{
+    class SmartCardPlayer : AI
+    {
+        public SmartCardPlayer(string name) : base(name){}
+
+        public override void Decision(bool isMyTurn, string cardSeeking, string fishFrom)
+        {
+            foreach (KeyValuePair<String, int> cardType in cards)
+            {
+                foreach (KeyValuePair<string, List<string>> publicCardsOfCardplayer in Globals.PUBLICLY_KNOWN_CARDS)
+                {
+                    if (!publicCardsOfCardplayer.Key.Equals(Name))
+                    {
+                        foreach (string public_knowncard in publicCardsOfCardplayer.Value)
+                        {
+                            if (cardType.Key.Equals(public_knowncard))
+                            {
+                                cardSeeking = public_knowncard;
+                                fishFrom = publicCardsOfCardplayer.Key;
+                                break;
+                            }
+                        }
+                    }
+                    if (!(cardSeeking == null && fishFrom == null)) { break; }
+                }
+                if (!(cardSeeking == null && fishFrom == null)) { break; }
+            }
+            base.Decision(isMyTurn, cardSeeking, fishFrom);
+        }
+    }
+}
+ 
