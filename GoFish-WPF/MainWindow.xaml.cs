2ea90e4d4792d3aabeb1f2dfd37ce71b987d1b96
Ranvir Sahota <ranvirsahota10@gmail.com>
2023-05-08 17:55:33 +0100

    updating and adding code to match console version
    Signed-off-by: Ranvir Sahota <ranvirsahota10@gmail.com>

--- GoFish-WPF/MainWindow.xaml.cs
+++ GoFish-WPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GoFishLibary;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +14,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace GoFish_WPF
 {
     /// <summary>
@@ -20,15 +21,106 @@ namespace GoFish_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<CardPlayer> cardPlayersSource;
+        public List<String> logSource;
+
         public MainWindow()
         {
-           List<String> names = new List<String>();
             InitializeComponent();
-            cardPlayers.ItemsSource = names;
-            names.Add("Ash");
-            names.Add("Archer");
-            names.Add("Pat");
+            //Create Players
+            Player player = new Player("You");
+            cardPlayersSource = new List<CardPlayer>() { new AI("Archer"), new AI("Patrick"), player};
+            cardPlayers.ItemsSource = cardPlayersSource;
 
+            
+            //Shuffle
+            List<String> cards = new List<String>()
+            {
+                "ace",
+                "ace",
+                "ace",
+                "ace",
+                "two",
+                "two",
+                "two",
+                "two",
+                "three",
+                "three",
+                "three",
+                "three",
+                "four",
+                "four",
+                "four",
+                "four",
+                "five",
+                "five",
+                "five",
+                "five",
+                "six",
+                "six",
+                "six",
+                "six",
+                "nine",
+                "nine",
+                "nine",
+                "nine",
+                "ten",
+                "ten",
+                "ten",
+                "ten",
+                "jack",
+                "jack",
+                "jack",
+                "jack",
+                "queen",
+                "queen",
+                "queen",
+                "queen",
+                "king",
+                "king",
+                "king",
+                "king",
+                "seven",
+                "seven",
+                "seven",
+                "seven",
+                "eight",
+                "eight",
+                "eight",
+                "eight",
+            };
+            var rnd = new Random();
+            cards = cards.OrderBy(x => rnd.Next()).ToList<string>();
+            
+            //Deal Cards
+            for (int counter = 1; counter <= 5; counter++)
+            {
+                foreach (CardPlayer cardPlayer in cardPlayersSource)
+                {
+                    cardPlayer.CardsSet(cards[0]);
+                    cards.RemoveAt(0);
+                }
+            }
+            hand.ItemsSource = player.getCards();
+            Globals.Stack = cards;
+            
+
+            //Set log
+            logSource = new List<String>();
+            logSource.Add($"Stack : {Globals.Stack}");
+            log.ItemsSource = logSource;
+
+            //Game Movement
+
+
+
+        }
+
+        private void fishFor_Click(object sender, RoutedEventArgs e)
+        {
+            CardPlayer? player = cardPlayers.SelectedItem as CardPlayer;
+            logSource.Add($"{player.Name}");
+            log.Items.Refresh();
         }
     }
 }
---
+++ GoFish-WPF/player.cs
@@ -0,0 +1,32 @@
+﻿using GoFishLibary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFish_WPF
+{
+    internal class Player : CardPlayer
+    {
+        public Player(string name)  : base(name) { Name = name; }
+
+        public override void Decsion(bool isMyTurn, string cardSeeking = null, string fishFrom = null)
+        {
+            base.Decsion(isMyTurn, cardSeeking, fishFrom);
+        }
+
+        public List<String> getCards()
+        {
+           List<String> cards = new List<String>();
+            foreach (KeyValuePair<String, int> card in _cards)
+            {
+                for (int i = 0; i < card.Value; i++)
+                {
+                    cards.Add(card.Key);
+                }
+            }
+            return cards;
+        }
+    }
+}
